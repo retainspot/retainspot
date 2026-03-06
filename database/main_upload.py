e688bd7b5434001ce8ff1a34c9a040c40061cbe1
unknown <luongthanhcong1802@gmail.com>
2026-03-06 12:25:41 -0500

    Create new table

--- database/main_upload.py
+++ database/main_upload.py
@@ -5,10 +5,10 @@ from database_utils import get_engine, upload_to_sql
 def main():
     engine = get_engine()
     df_customers = pd.read_csv('data/telco_churn_with_all_feedback.csv')
-    # df_behavior = pd.read_csv('data/behavior.csv')
+    df_behavior = pd.read_csv('data/data_with_churn_score.csv')
     
     upload_to_sql(df_customers, 'customers', engine)
-    # upload_to_sql(df_behavior, 'customer_behavior', engine)
+    upload_to_sql(df_behavior, 'customer_behavior', engine)
     
     print("Successfully ! ")
 
